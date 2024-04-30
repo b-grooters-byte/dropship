5e1c15e266d626b5ba1f4160b59fb7e3ef978b3b
b-grooters-byte <brandon.grooters@bytetrail.com>
2024-04-30 08:31:36 -0500

    basic velocity

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use bevy::prelude::*;
 const SHIP_START_Y: f32 = 200.0;
 const SHIP_START_X: f32 = 0.0;
 const SHIP_START_VELOCITY_X: f32 = 500.0;
-const SHIP_COLOR: Color = Color::rgb(0.8, 0.8, 1.0);
+const SHIP_COLOR: Color = Color::rgb(0.8, 0.8, 0.9);
 const SHIP_SIZE: Vec2 = Vec2::new(50.0, 50.0);
 
 fn main() {
@@ -11,7 +11,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, move_ship)
+        .add_systems(FixedUpdate, apply_velocity)
         .run();
 }
 
@@ -33,33 +33,36 @@ fn setup(mut commands: Commands) {
             ..default()
         },
         Ship,
+        Velocity(Vec2::new(0.0, 0.0)),
     ));
 }
 
-fn move_ship(
+fn apply_velocity(
     input: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
-    mut query: Query<(&Ship, &mut Transform)>,
+    mut query: Query<(&mut Transform, &mut Velocity)>,
 ) {
-    let mut paddle_transform = query.single_mut();
-    let mut direction = 0.0;
+    let mut ship_velocity = query.single_mut();
+    let mut delta_velocity = Vec2::new(0.0, 0.0);
 
     if input.pressed(KeyCode::ArrowLeft) {
-        direction -= 1.0;
+        delta_velocity.x -= 100.0;
     } else if input.pressed(KeyCode::ArrowRight) {
-        direction += 1.0;
+        delta_velocity.x += 100.0;
+    } else if input.pressed(KeyCode::ArrowUp) {
+        delta_velocity.y += 100.0;
+    } else if input.pressed(KeyCode::ArrowDown) {
+        delta_velocity.y -= 100.0;
     }
 
-    let new_x =
-        paddle_transform.1.translation.x + direction * SHIP_START_VELOCITY_X * time.delta_seconds();
-    paddle_transform.1.translation.x = new_x;
+    ship_velocity.1 .0 += delta_velocity * time.delta_seconds();
+
+    ship_velocity.0.translation.x += ship_velocity.1 .0.x * time.delta_seconds();
+    ship_velocity.0.translation.y += ship_velocity.1 .0.y * time.delta_seconds();
 }
 
 #[derive(Component)]
 struct Ship;
 
 #[derive(Component)]
-struct Velocity {
-    x: f32,
-    y: f32,
-}
+struct Velocity(Vec2);
