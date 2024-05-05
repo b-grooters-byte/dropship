86b293ed42efebf8d437d30aa7be30b1509aedb5
Brandon Grooters <brandon.grooters@bytetrail.com>
2024-05-05 10:25:51 -0500

    adding real world consts

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,17 @@ use bevy::prelude::*;
 const SHIP_START_Y: f32 = 0.0;
 const SHIP_START_X: f32 = 0.0;
 const SHIP_START_VELOCITY_X: f32 = 500.0;
-const SHIP_COLOR: Color = Color::rgb(0.8, 0.8, 0.9);
-const SHIP_SIZE: Vec2 = Vec2::new(50.0, 50.0);
+
+const LANDER_MASS: f32 = 15_000.0;
+const LANDER_FUEL_MASS: f32 = 5_000.0;
+const LANDER_THRUST_MAX: f32 = 33_750.0;
+const LANDER_THRUST_MIN: f32 = 4_500.0;
+
+const LUNAR_GRAVITY: f32 = 1.625;
+const EUROPA_GRAVITY: f32 = 1.314;
+const IO_GRAVITY: f32 = 1.796;
+const CALLISTO_GRAVITY: f32 = 1.235;
+const GANYMEDE_GRAVITY: f32 = 1.428;
 
 fn main() {
     App::new()
@@ -42,6 +51,9 @@ fn apply_velocity(
     let mut ship_velocity = query.single_mut();
     let mut delta_velocity = Vec2::new(0.0, 0.0);
 
+    // caclulate acceleration due to gravity
+    let gravity_accel = LUNAR_GRAVITY * 10.0;
+    delta_velocity.y -= gravity_accel;
     if input.pressed(KeyCode::ArrowLeft) {
         delta_velocity.x -= 100.0;
     } else if input.pressed(KeyCode::ArrowRight) {
@@ -51,9 +63,7 @@ fn apply_velocity(
     } else if input.pressed(KeyCode::ArrowDown) {
         delta_velocity.y -= 100.0;
     }
-
     ship_velocity.1 .0 += delta_velocity * time.delta_seconds();
-
     ship_velocity.0.translation.x += ship_velocity.1 .0.x * time.delta_seconds();
     ship_velocity.0.translation.y += ship_velocity.1 .0.y * time.delta_seconds();
 }
