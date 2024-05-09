01dd3b40e485eb525a949810a38790087004c58b
Brandon Grooters <brandon.grooters@bytetrail.com>
2024-05-08 20:33:36 -0500

    rcs/thrust

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,29 @@ const SHIP_START_Y: f32 = 0.0;
 const SHIP_START_X: f32 = 0.0;
 const SHIP_START_VELOCITY_X: f32 = 500.0;
 
+// mass in kg
 const LANDER_MASS: f32 = 15_000.0;
+// primary fuel mass for main engine
 const LANDER_FUEL_MASS: f32 = 5_000.0;
+// main engine maximum thrust in newtons
 const LANDER_THRUST_MAX: f32 = 33_750.0;
+// main engine minimum thrust in newtons
 const LANDER_THRUST_MIN: f32 = 4_500.0;
-
+// gravity in m/s/s
 const LUNAR_GRAVITY: f32 = 1.625;
 const EUROPA_GRAVITY: f32 = 1.314;
 const IO_GRAVITY: f32 = 1.796;
 const CALLISTO_GRAVITY: f32 = 1.235;
 const GANYMEDE_GRAVITY: f32 = 1.428;
 
+const RCS_STEP: f32 = 0.523598775598299;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, (apply_rcs, apply_velocity))
+        .add_systems(FixedUpdate, (apply_thrust, apply_velocity))
         .run();
 }
 
@@ -54,21 +60,33 @@ mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,) {
     ));
 }
 
-fn apply_rcs(
+fn apply_thrust(
+    time: Res<Time>,
     input: Res<ButtonInput<KeyCode>>,
     mut query: Query<(&mut Transform, &mut Rcs, &mut TextureAtlas)>,
 ) {
     let mut ship = query.single_mut();
+    
     let mut texture_atlas = ship.2;
-
+    let mut rcs = ship.1;
+    let mut delta_angle = 0.0; 
     if input.pressed(KeyCode::ArrowLeft) {
-        texture_atlas.index = 2;
-    } else if input.pressed(KeyCode::ArrowRight) {
         texture_atlas.index = 4;
+        delta_angle += time.delta_seconds() * RCS_STEP;
+        rcs.0 = Thruster::Left;
+    } else if input.pressed(KeyCode::ArrowRight) {
+        texture_atlas.index = 2;
+        rcs.0 = Thruster::Right;
+        delta_angle -= time.delta_seconds() * RCS_STEP;
     } else {
         texture_atlas.index = 0;
+        rcs.0 = Thruster::None;
+    }
+    if input.pressed(KeyCode::ArrowUp) {
+        texture_atlas.index += 1;
     }
 
+    ship.0.rotate_z(delta_angle);
 }
 
 fn apply_velocity(
@@ -84,10 +102,9 @@ fn apply_velocity(
     delta_velocity.y -= gravity_accel;
     if input.pressed(KeyCode::ArrowUp) {
         delta_velocity.y += 100.0;
-    } else if input.pressed(KeyCode::ArrowDown) {
-        delta_velocity.y -= 100.0;
-    }
+    } 
     ship_velocity.1 .0 += delta_velocity * time.delta_seconds();
+
     ship_velocity.0.translation.x += ship_velocity.1 .0.x * time.delta_seconds();
     ship_velocity.0.translation.y += ship_velocity.1 .0.y * time.delta_seconds();
 }
